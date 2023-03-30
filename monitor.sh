2c8512c2c5353ef1eec8abcaf3ef4af3d6738370
carina0329 <53321343+carina0329@users.noreply.github.com>
2023-03-29 22:10:17 -0500

    update the script

--- monitor.sh
+++ monitor.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # Create headers in the CSV file
-echo "Date and Time,CPU Usage (%),Disk Usage (%)" > usage.csv
+echo "Timestamp,CPU Usage (%),Disk Usage (%)" > usage.csv
 
 while true; do
     cpu=$(top -l 1 -n 1 -stats "cpu" | awk '/^CPU usage:/{print $3+$5}')
     disk=$(df -h | awk 'NR==2{print $5}')
-    timestamp=$(date +"%Y/%m/%d %H:%M:%S")
+    timestamp=$(date +%s)
     echo "$timestamp,$cpu%,$disk" >> usage.csv
-    sleep 2
+    sleep 0.01
 done
