bd15cb6974a2b9448a51c79caec93d5813e61640
Rahul Sai <rs.rahulsai23@gmail.com>
2023-05-01 14:42:56 -0500

    Gitignore updates

--- rocksdb/setup.sh
+++ rocksdb/setup.sh
@@ -1,7 +1,7 @@
 # !/bin/bash
 
 sudo apt-get update -y
-sudo apt-get install build-essential libsnappy-dev zlib1g-dev libbz2-dev libgflags-dev
+sudo apt-get install build-essential libsnappy-dev zlib1g-dev libbz2-dev libgflags-dev -y
 git clone https://github.com/facebook/rocksdb.git
 cd rocksdb
 sudo make static_lib
