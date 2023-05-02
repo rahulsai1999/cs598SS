ca8812a5dd4228573fa411dd5e3817f7b87489a2
carina0329 <53321343+carina0329@users.noreply.github.com>
2023-05-02 15:36:07 -0500

    changed the script to write throughput data to disk_throughput.csv

--- disk_util.sh
+++ disk_util.sh
@@ -5,7 +5,5 @@ OUTPUT_FILE="disk_throughput.csv"
 
 echo "Time, Bytes_Written" > $OUTPUT_FILE
 
-DISK_FILE="disk_stats.txt"
-
 # Run iostat once and write to a temporary file
-iostat -d 1 | awk -W interactive -v date="$(date +"%Y-%m-%d %H:%M:%S")" '$1=="nvme0n1"{ print date, $NF;fflush();}' >> DISK_FILE
+iostat -d 1 | awk -W interactive -v date="$(date +"%Y-%m-%d %H:%M:%S")" '$1=="nvme0n1"{ print date, $NF;fflush();}' >> $OUTPUT_FILE
