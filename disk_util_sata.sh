e1d649e92fe543a5298f39bf6500fc6389b93151
carina0329 <53321343+carina0329@users.noreply.github.com>
2023-05-02 16:54:44 -0500

    modified the scripts to take in number of client threads as an argument and append it to output filename

--- disk_util_sata.sh
+++ disk_util_sata.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # Set output file name
-OUTPUT_FILE="disk_throughput.csv"
+NUM_CLIENTS=${1:-1}
+OUTPUT_FILE="disk_throughput_${NUM_CLIENTS}.csv"
 
 echo "Time, KB_Written" > $OUTPUT_FILE
 # Run iostat once and write to a temporary file
