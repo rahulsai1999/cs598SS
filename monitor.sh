8516434468cad853caaf91d847916c1c45a927bf
rsrw23 <rsrw23@node2.cds-test-2.cs598cds-rsr4-pg0.utah.cloudlab.us>
2023-05-01 22:05:24 -0600

    fixed script on NVMe to include wkb/s and rkb/s

--- monitor.sh
+++ monitor.sh
@@ -7,7 +7,7 @@ OUTPUT_FILE3="utilization.csv"
 
 echo "Time,Read/s,Write/s,util%" > $OUTPUT_FILE1
 echo "Time, cpu%" > $OUTPUT_FILE2
-echo "Time,Read/s,Write/s,util%,cpu%" > $OUTPUT_FILE3
+echo "Time,ReadKB/s,WriteKB/s,util%,cpu%" > $OUTPUT_FILE3
 
 DISK_FILE="disk_stats.txt"
 
@@ -21,7 +21,7 @@ while true; do
     # get cpu utilization using top
     cpu=$(mpstat 1 1 | awk '/^Average:/ {print 100-$NF}')
     echo "$current_time,$cpu" >> $OUTPUT_FILE2
-    latest_disk_utilization=$(tail -2 "$DISK_FILE" | awk -W interactive '$1=="sda"{ print $2,",",$3,",",$NF;fflush();}')
+    latest_disk_utilization=$(tail -2 "$DISK_FILE" | awk -W interactive '$1=="nvme0n1"{ print $4,",",$5,",",$NF;fflush();}')
     echo "$current_time,$latest_disk_utilization" >> $OUTPUT_FILE1
     echo "$current_time,$latest_disk_utilization,$cpu" >> $OUTPUT_FILE3
     # Get disk utilization using iostat and append data to the output file with timestamp
