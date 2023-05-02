2c3e66d52d2f753346f2983b7f604eaa2f813779
carina0329 <53321343+carina0329@users.noreply.github.com>
2023-05-02 15:48:34 -0500

    changed disk util to use gawk

--- disk_util.sh
+++ disk_util.sh
@@ -4,6 +4,5 @@
 OUTPUT_FILE="disk_throughput.csv"
 
 echo "Time, Bytes_Written" > $OUTPUT_FILE
-
 # Run iostat once and write to a temporary file
-iostat -d 1 | awk -W interactive -v date="$(date +"%Y-%m-%d %H:%M:%S")" '$1=="nvme0n1"{ print date, $NF;fflush();}' >> $OUTPUT_FILE
+iostat -d 1 | gawk '$1=="nvme0n1"{ print strftime("[%Y-%m-%d %H:%M:%S]"), $NF }'  >> $OUTPUT_FILE
