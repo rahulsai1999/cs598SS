a96a66ca58d8c235b9aebdeb1c5d5ee740aff380
Rahul Sai <rs.rahulsai23@gmail.com>
2023-05-01 22:27:41 -0500

    Wiredtiger automating 2

--- wiredtiger/run.sh
+++ wiredtiger/run.sh
@@ -6,4 +6,5 @@ cp ex_access.c wiredtiger/examples/c/
 cp ../ycsb/ycsb-0.17.0/$workloadName wiredtiger/build/examples/c/ex_access/
 cd wiredtiger/build/examples/c
 make ex_access
-./ex_access/ex_access $workloadName
+cd ex_access
+./ex_access $workloadName
