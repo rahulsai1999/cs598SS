9a90d5889f2525945b0ae5ba8eda93d01fb85d5a
carina0329 <53321343+carina0329@users.noreply.github.com>
2023-05-02 16:00:56 -0500

    fixing disk_util to write to csv

--- disk_util.sh
+++ disk_util.sh
@@ -3,6 +3,6 @@
 # Set output file name
 OUTPUT_FILE="disk_throughput.csv"
 
-echo "Time, Bytes_Written" > $OUTPUT_FILE
+echo "Time, KB_Written" > $OUTPUT_FILE
 # Run iostat once and write to a temporary file
-iostat -d 1 | gawk '$1=="nvme0n1"{ print strftime("[%Y-%m-%d %H:%M:%S]"), $NF }'  >> $OUTPUT_FILE
+iostat -d 1 | gawk '$1=="nvme0n1"{ print strftime("[%Y-%m-%d %H:%M:%S]"), $NF;fflush() }'  >> $OUTPUT_FILE
