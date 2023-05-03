fe6988ad44e4972158d4d9e6d8c72622a40533fb
Rahul Sai <rs.rahulsai23@gmail.com>
2023-05-02 21:16:54 -0500

    Modify splinter shell script

--- splinterdb/run.sh
+++ splinterdb/run.sh
@@ -1,12 +1,13 @@
 # !/bin/bash
 
 workloadName=$1
+myfilesize=$(wc -c $workloadName | awk '{print $1}')
 threads=$2
-let split=$threads+1
+let split=$myfilesize/$threads
 
 cp splinterdb_intro_example.c splinterdb/examples/
 cp ../ycsb/ycsb-0.17.0/$workloadName splinterdb/examples/
 cd splinterdb/examples/
-split -n l/$split  outWorkloadA.txt
+split -b $split $workloadName
 gcc splinterdb_intro_example.c -DSPLINTERDB_PLATFORM_DIR=platform_linux -lsplinterdb -lpthread
 ./a.out $threads
